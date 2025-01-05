82cb42c208b905f5995b5662f0a2e98753d88428
Max Drexel <max-github@drexel.dev>
2025-01-05 01:02:26 -0500

    Add sln and project

---
+++ src/Drexel.Host.Dns.Cloudflare/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Drexel.Host
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
