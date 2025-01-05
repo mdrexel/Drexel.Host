7ba32236c5b957c9b57389d42de294cc8ca352f5
Max Drexel <max-github@drexel.dev>
2025-01-05 03:38:53 -0500

    Playing around with `System.CommandLine` which I randomly stumbled across while researching AOT/publishing

--- src/Drexel.Host/Program.cs
+++ src/Drexel.Host/Program.cs
@@ -1,13 +1,56 @@
 ﻿using System;
+using System.CommandLine;
+using System.CommandLine.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Drexel.Host
 {
     public class Program
     {
-        public static void Main(string[] args)
+        private static Option<string> UrlOption { get; } = new("--url", "A URL.");
+
+        public static async Task<int> Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.ReadLine();
+            RootCommand rootCommand =
+                new("Max experimenting")
+                {
+                    UrlOption,
+                };
+
+            rootCommand.SetHandler(
+                static async (context) =>
+                {
+                    string? urlOptionValue = context.ParseResult.GetValueForOption(UrlOption);
+                    CancellationToken token = context.GetCancellationToken();
+                    context.ExitCode = await DoRootCommand(context.Console, urlOptionValue, token);
+                });
+
+            return await rootCommand.InvokeAsync(args);
+        }
+
+        public static async Task<int> DoRootCommand(
+            IConsole console,
+            string? urlOptionValue,
+            CancellationToken cancellationToken)
+        {
+            try
+            {
+                using HttpClient httpClient = new();
+
+                HttpResponseMessage response = await httpClient.GetAsync(urlOptionValue, cancellationToken);
+                string content = await response.Content.ReadAsStringAsync(cancellationToken);
+
+                console.WriteLine(content);
+
+                return 0;
+            }
+            catch (OperationCanceledException e)
+            {
+                console.Error.WriteLine(e.Message);
+                return 1;
+            }
         }
     }
 }
