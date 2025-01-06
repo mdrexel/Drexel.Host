6518de7f91e8d5d36e57e89065ae70fd30a1cdc5
Max Drexel <max-github@drexel.dev>
2025-01-06 03:15:54 -0500

    Add some goofy spacing to try to make this one line not egregiously long, and remove leftover comment

--- src/Drexel.Host/Internals/Command.cs
+++ src/Drexel.Host/Internals/Command.cs
@@ -13,14 +13,18 @@ namespace Drexel.Host.Internals
     /// <typeparam name="THandler">
     /// The type that implements the command callback.
     /// </typeparam>
-    internal abstract class Command<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicProperties | DynamicallyAccessedMemberTypes.PublicConstructors)] TOptions, [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] THandler> : Command
+    internal abstract class
+        Command<
+            [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicProperties | DynamicallyAccessedMemberTypes.PublicConstructors)] TOptions,
+            [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] THandler>
+        : Command
         where THandler : ICommandHandler<TOptions, THandler>
     {
         /// <inheritdoc cref="Command(string, string?)"/>
         protected Command(string name, string description)
             : base(name, description)
         {
-            this.Handler = CommandHandler.Create</*InvocationContext, */ TOptions, IServiceProvider, CancellationToken>(
+            this.Handler = CommandHandler.Create<TOptions, IServiceProvider, CancellationToken>(
                 static async (options, serviceProvider, cancellationToken) =>
                 {
                     THandler handler = THandler.Create(serviceProvider);
