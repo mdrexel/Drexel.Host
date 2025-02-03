4dec116fb591a63a31fc0513cd10f3351fff109f
Max Drexel <max-github@drexel.dev>
2025-02-02 22:33:44 -0500

    Start working on power commands. I don't like that the platform is being detected at runtime rather than compile-time; I'd prefer adding support for a new platform produces compile-time errors). But apparently this is the "recommended" solution, even though internally Microsoft is using preprocessor constants to do a compile-time check and baking the result into the .NET runtime assemblies...

---
+++ src/Drexel.Host/Commands/Power/Off/PowerOffCommand.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Runtime.Versioning;
+using System.Threading;
+using System.Threading.Tasks;
+using Drexel.Host.Internals;
+using Spectre.Console;
+
+namespace Drexel.Host.Commands.Power.Off
+{
+    /// <summary>
+    /// Performs a power-off operation.
+    /// </summary>
+    internal sealed class PowerOffCommand : Command<PowerOffCommand.Options, PowerOffCommand.Handler>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PowerOffCommand"/> class.
+        /// </summary>
+        public PowerOffCommand()
+            : base("off", "Performs a power-off operation.")
+        {
+        }
+
+        /// <summary>
+        /// The options associated with performing the command.
+        /// </summary>
+        public new sealed class Options
+        {
+        }
+
+        /// <summary>
+        /// The command implementation.
+        /// </summary>
+        /// <param name="console">
+        /// The console to use.
+        /// </param>
+        public new sealed class Handler(IAnsiConsole console) : ICommandHandler<Options, Handler>
+        {
+            /// <inheritdoc/>
+            public async Task<int> HandleAsync(Options options, CancellationToken cancellationToken)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                if (OperatingSystem.IsWindows())
+                {
+                    return await ShutdownWindowsAsync();
+                }
+                else if (OperatingSystem.IsLinux())
+                {
+                    return await ShutdownLinuxAsync();
+                }
+                else
+                {
+                    throw new PlatformNotSupportedException(
+                        "The current platform does not have a shutdown implementation defined.");
+                }
+            }
+
+            [SupportedOSPlatform("windows")]
+            private static async Task<int> ShutdownWindowsAsync()
+            {
+                throw new NotImplementedException();
+            }
+
+            [SupportedOSPlatform("linux")]
+            private static async Task<int> ShutdownLinuxAsync()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ src/Drexel.Host/Commands/Power/PowerRoot.cs
@@ -0,0 +1,20 @@
+﻿using System.CommandLine;
+using Drexel.Host.Commands.Power.Off;
+
+namespace Drexel.Host.Commands.Power
+{
+    /// <summary>
+    /// The <c>power</c> command root.
+    /// </summary>
+    public sealed class PowerRoot : Command
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PowerRoot"/> class.
+        /// </summary>
+        public PowerRoot()
+            : base("http", "Power-related actions.")
+        {
+            this.Add(new PowerOffCommand());
+        }
+    }
+}
