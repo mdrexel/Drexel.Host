dec9e13b3e3b4e3a5954074abd1361587f37f05f
Max Drexel <max-github@drexel.dev>
2025-02-03 02:01:48 -0500

    Working on reboot (this doesn't actually belong here I need shutdown but I'll also need reboot)

--- src/Drexel.Host/Commands/Power/Off/PowerOffCommand.cs
+++ src/Drexel.Host/Commands/Power/Off/PowerOffCommand.cs
@@ -117,11 +117,11 @@ namespace Drexel.Host.Commands.Power.Off
 
                 if (OperatingSystem.IsWindowsVersionAtLeast(5, 1, 2600))
                 {
-                    return await ShutdownWindowsAsync(options, cancellationToken);
+                    return await new Windows(console).ShutdownAsync(options, cancellationToken);
                 }
                 else if (OperatingSystem.IsLinux())
                 {
-                    return await ShutdownLinuxAsync(options, cancellationToken);
+                    return await new Linux(console).ShutdownAsync(options, cancellationToken);
                 }
                 else
                 {
@@ -130,131 +130,172 @@ namespace Drexel.Host.Commands.Power.Off
                 }
             }
 
-            [SupportedOSPlatform("windows5.1.2600")]
-            private async Task<int> ShutdownWindowsAsync(
-                Options options,
-                CancellationToken cancellationToken)
+            private sealed class Windows(IAnsiConsole console)
             {
-                cancellationToken.ThrowIfCancellationRequested();
-
-                EXIT_WINDOWS_FLAGS mode = options.Force
-                    ? EXIT_WINDOWS_FLAGS.EWX_POWEROFF | EXIT_WINDOWS_FLAGS.EWX_FORCE
-                    : EXIT_WINDOWS_FLAGS.EWX_POWEROFF;
-                SHUTDOWN_REASON reason = Convert(options.Reason);
-
-                const short TOKEN_ADJUST_PRIVILEGES = 32;
-                const short TOKEN_QUERY = 8;
-                if (0 == OpenProcessToken(
-                    Process.GetCurrentProcess().Handle,
-                    TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY,
-                    out IntPtr hToken))
+                [SupportedOSPlatform("windows5.1.2600")]
+                public async Task<int> ShutdownAsync(
+                    Options options,
+                    CancellationToken cancellationToken)
                 {
-                    console.WriteException(
-                        new Exception(Marshal.GetLastPInvokeErrorMessage()),
-                        ExceptionFormats.NoStackTrace);
-                    return Marshal.GetLastPInvokeError();
-                }
+                    cancellationToken.ThrowIfCancellationRequested();
 
-                if (0 == LookupPrivilegeValue("", "SeShutdownPrivilege", out LUID lpLuid))
-                {
-                    console.WriteException(
-                        new Exception(Marshal.GetLastPInvokeErrorMessage()),
-                        ExceptionFormats.NoStackTrace);
-                    return Marshal.GetLastPInvokeError();
-                }
+                    EXIT_WINDOWS_FLAGS mode = options.Force
+                        ? EXIT_WINDOWS_FLAGS.EWX_POWEROFF | EXIT_WINDOWS_FLAGS.EWX_FORCE
+                        : EXIT_WINDOWS_FLAGS.EWX_POWEROFF;
+                    SHUTDOWN_REASON reason = Convert(options.Reason);
 
-                const short SE_PRIVILEGE_ENABLED = 2;
-                TOKEN_PRIVILEGES tkp =
-                    new()
-                    {
-                        PrivilegeCount = 1,
-                        Privileges =
-                            {
-                                pLuid = lpLuid,
-                                Attributes = SE_PRIVILEGE_ENABLED,
-                            },
-                    };
-
-                if (!AdjustTokenPrivileges(
-                    hToken,
-                    false,
-                    ref tkp,
-                    0U,
-                    IntPtr.Zero,
-                    IntPtr.Zero))
-                {
-                    console.WriteException(
-                        new Exception(Marshal.GetLastPInvokeErrorMessage()),
-                        ExceptionFormats.NoStackTrace);
-                    return Marshal.GetLastPInvokeError();
-                }
-
-                if (!options.WhatIf)
-                {
-                    if (!PInvoke.ExitWindowsEx(mode, reason))
+                    const short TOKEN_ADJUST_PRIVILEGES = 32;
+                    const short TOKEN_QUERY = 8;
+                    if (0 == OpenProcessToken(
+                        Process.GetCurrentProcess().Handle,
+                        TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY,
+                        out IntPtr hToken))
                     {
                         console.WriteException(
                             new Exception(Marshal.GetLastPInvokeErrorMessage()),
                             ExceptionFormats.NoStackTrace);
                         return Marshal.GetLastPInvokeError();
                     }
+
+                    if (0 == LookupPrivilegeValue("", "SeShutdownPrivilege", out LUID lpLuid))
+                    {
+                        console.WriteException(
+                            new Exception(Marshal.GetLastPInvokeErrorMessage()),
+                            ExceptionFormats.NoStackTrace);
+                        return Marshal.GetLastPInvokeError();
+                    }
+
+                    const short SE_PRIVILEGE_ENABLED = 2;
+                    TOKEN_PRIVILEGES tkp =
+                        new()
+                        {
+                            PrivilegeCount = 1,
+                            Privileges =
+                                {
+                                pLuid = lpLuid,
+                                Attributes = SE_PRIVILEGE_ENABLED,
+                                },
+                        };
+
+                    if (!AdjustTokenPrivileges(
+                        hToken,
+                        false,
+                        ref tkp,
+                        0U,
+                        IntPtr.Zero,
+                        IntPtr.Zero))
+                    {
+                        console.WriteException(
+                            new Exception(Marshal.GetLastPInvokeErrorMessage()),
+                            ExceptionFormats.NoStackTrace);
+                        return Marshal.GetLastPInvokeError();
+                    }
+
+                    if (!options.WhatIf)
+                    {
+                        if (!PInvoke.ExitWindowsEx(mode, reason))
+                        {
+                            console.WriteException(
+                                new Exception(Marshal.GetLastPInvokeErrorMessage()),
+                                ExceptionFormats.NoStackTrace);
+                            return Marshal.GetLastPInvokeError();
+                        }
+                    }
+
+                    return 0;
+
+                    static SHUTDOWN_REASON Convert(Reason reason) =>
+                        reason switch
+                        {
+                            Reason.None => SHUTDOWN_REASON.SHTDN_REASON_NONE,
+                            Reason.Power => SHUTDOWN_REASON.SHTDN_REASON_MAJOR_POWER,
+                            Reason.Software => SHUTDOWN_REASON.SHTDN_REASON_MAJOR_SOFTWARE,
+                            Reason.Hardware => SHUTDOWN_REASON.SHTDN_REASON_MAJOR_HARDWARE,
+                            _ => throw new ArgumentException("Unrecognized power-off reason.", nameof(options)),
+                        };
                 }
 
-                return 0;
+                [DllImport("advapi32.dll")]
+                private static extern int OpenProcessToken(IntPtr ProcessHandle, int DesiredAccess, out IntPtr TokenHandle);
 
-                static SHUTDOWN_REASON Convert(Reason reason) =>
-                    reason switch
+                [DllImport("advapi32.dll", SetLastError = true)]
+                [return: MarshalAs(UnmanagedType.Bool)]
+                private static extern bool AdjustTokenPrivileges(
+                    IntPtr TokenHandle,
+                    [MarshalAs(UnmanagedType.Bool)] bool DisableAllPrivileges,
+                    ref TOKEN_PRIVILEGES NewState,
+                    UInt32 BufferLength,
+                    IntPtr PreviousState,
+                    IntPtr ReturnLength);
+
+                [DllImport("advapi32.dll")]
+                private static extern int LookupPrivilegeValue(string lpSystemName, string lpName, out LUID lpLuid);
+
+                private struct LUID
+                {
+                    public int LowPart;
+                    public int HighPart;
+                }
+
+                private struct LUID_AND_ATTRIBUTES
+                {
+                    public LUID pLuid;
+                    public int Attributes;
+                }
+
+                private struct TOKEN_PRIVILEGES
+                {
+                    public int PrivilegeCount;
+                    public LUID_AND_ATTRIBUTES Privileges;
+                }
+            }
+
+            private sealed class Linux(IAnsiConsole console)
+            {
+                [SupportedOSPlatform("linux")]
+                public async Task<int> ShutdownAsync(
+                    Options options,
+                    CancellationToken cancellationToken)
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+
+                    int result = reboot(LINUX_REBOOT_CMD_POWER_OFF, IntPtr.Zero);
+                    if (result != -1)
                     {
-                        Reason.None => SHUTDOWN_REASON.SHTDN_REASON_NONE,
-                        Reason.Power => SHUTDOWN_REASON.SHTDN_REASON_MAJOR_POWER,
-                        Reason.Software => SHUTDOWN_REASON.SHTDN_REASON_MAJOR_SOFTWARE,
-                        Reason.Hardware => SHUTDOWN_REASON.SHTDN_REASON_MAJOR_HARDWARE,
-                        _ => throw new ArgumentException("Unrecognized power-off reason.", nameof(options)),
-                    };
-            }
+                        console.WriteException(
+                            new Exception("Unexpected return code."),
+                            ExceptionFormats.NoStackTrace);
+                        return result;
+                    }
 
-            [SupportedOSPlatform("linux")]
-            private async Task<int> ShutdownLinuxAsync(
-                Options options,
-                CancellationToken cancellationToken)
-            {
-                cancellationToken.ThrowIfCancellationRequested();
+                    result = Marshal.GetLastWin32Error();
+                    switch (result)
+                    {
+                        case EPERM:
+                            throw new UnauthorizedAccessException("Don't have permissions to reboot.");
+                        case EINVAL:
+                            throw new InvalidOperationException("Bad input values.");
+                        case EFAULT:
+                        default:
+                            throw new InvalidOperationException("Could not call reboot:" + result.ToString());
+                    }
+                }
 
-                throw new NotImplementedException();
-            }
+                [DllImport("libc.so", SetLastError = true)]
+                public static extern Int32 reboot(Int32 cmd, IntPtr arg);
 
-            [DllImport("advapi32.dll")]
-            static extern int OpenProcessToken(IntPtr ProcessHandle, int DesiredAccess, out IntPtr TokenHandle);
+                public const Int32 LINUX_REBOOT_CMD_RESTART = 0x01234567;
+                public const Int32 LINUX_REBOOT_CMD_HALT = unchecked((int)0xCDEF0123);
+                public const Int32 LINUX_REBOOT_CMD_CAD_ON = unchecked((int)0x89ABCDEF);
+                public const Int32 LINUX_REBOOT_CMD_CAD_OFF = 0x00000000;
+                public const Int32 LINUX_REBOOT_CMD_POWER_OFF = 0x4321FEDC;
+                public const Int32 LINUX_REBOOT_CMD_RESTART2 = unchecked((int)0xA1B2C3D4);
+                public const Int32 LINUX_REBOOT_CMD_SW_SUSPEND = unchecked((int)0xD000FCE2);
+                public const Int32 LINUX_REBOOT_CMD_KEXEC = 0x45584543;
 
-            [DllImport("advapi32.dll", SetLastError = true)]
-            [return: MarshalAs(UnmanagedType.Bool)]
-            static extern bool AdjustTokenPrivileges(
-                IntPtr TokenHandle,
-                [MarshalAs(UnmanagedType.Bool)] bool DisableAllPrivileges,
-                ref TOKEN_PRIVILEGES NewState,
-                UInt32 BufferLength,
-                IntPtr PreviousState,
-                IntPtr ReturnLength);
-
-            [DllImport("advapi32.dll")]
-            static extern int LookupPrivilegeValue(string lpSystemName, string lpName, out LUID lpLuid);
-
-            private struct LUID
-            {
-                public int LowPart;
-                public int HighPart;
-            }
-
-            private struct LUID_AND_ATTRIBUTES
-            {
-                public LUID pLuid;
-                public int Attributes;
-            }
-
-            private struct TOKEN_PRIVILEGES
-            {
-                public int PrivilegeCount;
-                public LUID_AND_ATTRIBUTES Privileges;
+                public const Int32 EPERM = 1;
+                public const Int32 EFAULT = 14;
+                public const Int32 EINVAL = 22;
             }
         }
     }
